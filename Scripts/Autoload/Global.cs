584d7e7151fed4b22c7b6b53b61d7505d70bc388
Vasulvius <leurond.raphael@gmail.com>
2024-10-09 13:57:34 +0200

    Added night/day cycle basics

--- Scripts/Autoload/Global.cs
+++ Scripts/Autoload/Global.cs
@@ -3,11 +3,19 @@ using System;
 
 public partial class Global : Node
 {
+	// Singleton vars
+	public static Global Instance { get; private set; }
+
+	// Ressources vars
 	public int plantSelected = 1; // 1 for carrot, 2 for lettuce
 	public int numOfCarrots = 0;
 	public int numOfOnions = 0;
 	public int numOfRedPotatoe = 0;
-	public static Global Instance { get; private set; }
+
+	// Time vars
+	public enum TimePeriod {DAY, NIGHT};
+	public TimePeriod timePeriod;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
---
+++ Scripts/Autoload/TimeCycleManager.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public partial class TimeCycleManager : Node2D
+{
+	// Singleton vars
+	public static TimeCycleManager Instance { get; private set; }
+
+	// Time vars
+	[Export] private Timer dayTimer;
+	[Export] private Timer nightTimer;
+
+	// Display vars
+	[Export] private CanvasLayer nightCanvas;
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Create this as a singleton
+		Instance = this;
+		LaunchTimeCycle();
+	}
+
+	private async void LaunchTimeCycle()
+	{
+		if(Global.Instance.timePeriod == Global.TimePeriod.DAY)
+		{
+			nightCanvas.Visible = false;
+			dayTimer.Start();
+			await ToSignal(dayTimer, "timeout");
+			Global.Instance.timePeriod = Global.TimePeriod.NIGHT;
+			// GD.Print("Night start");
+			LaunchTimeCycle();
+		}
+		else
+		{
+			nightCanvas.Visible = true;
+			nightTimer.Start();
+			await ToSignal(nightTimer, "timeout");
+			Global.Instance.timePeriod = Global.TimePeriod.DAY;
+			// GD.Print("Day start");
+			LaunchTimeCycle();
+		}
+	}
+}
