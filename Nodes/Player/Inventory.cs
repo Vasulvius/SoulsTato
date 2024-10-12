b1a04988173bba85818499d66975a7599a6f9f35
Vasulvius <leurond.raphael@gmail.com>
2024-10-12 18:51:33 +0200

    Created ObjectDataBase

--- Nodes/Player/Inventory.cs
+++ Nodes/Player/Inventory.cs
@@ -1,11 +1,16 @@
 using Godot;
 using System;
 
-public partial class Inventory : Node2D
+public partial class Inventory : Node
 {
+	// Singleton vars
+	public static Inventory Instance { get; private set; }
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Create this as a singleton
+		Instance = this;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
