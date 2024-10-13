3deb1793aacb629507e90a000ac94dc42a3438c8
Vasulvius <leurond.raphael@gmail.com>
2024-10-13 16:01:42 +0200

    Changed items.JSON + files depends on

--- Scripts/Autoload/ObjectDataBase.cs
+++ Scripts/Autoload/ObjectDataBase.cs
@@ -9,7 +9,8 @@ public partial class ObjectDataBase : Node
 	public static ObjectDataBase Instance { get; private set; }
 
 	// Data vars
-	public Dictionary<string, string> items {get; private set;}
+	public Dictionary<string, Dictionary> items {get; private set;}
+	public Dictionary<string, string> itemsID {get; private set;}
 	private const string ITEMS_JSON_PATH = "res://Data/Items.json";
 
 	// Called when the node enters the scene tree for the first time.
@@ -17,23 +18,36 @@ public partial class ObjectDataBase : Node
 	{
 		// Create this as a singleton
 		Instance = this;
+		// Get the items data
 		items = ReadItemsData();
+		// Create a quick dictionary about ID and item's path
+		itemsID = CreateItemsID_Dict();
 	}
 
-	private Dictionary<string, string> ReadItemsData()
+	private Dictionary<string, Dictionary> ReadItemsData()
 	{
 		FileAccess files = FileAccess.Open(ITEMS_JSON_PATH, FileAccess.ModeFlags.Read);
 		string text = files.GetAsText();
-		Dictionary<string, string> jsonFile = (Dictionary<string, string>)Json.ParseString(text);
+		Dictionary<string, Dictionary> jsonFile = (Dictionary<string, Dictionary>)Json.ParseString(text);
 		files.Close();
 
 		return jsonFile;
 	}
 
+	private Dictionary<string, string> CreateItemsID_Dict()
+	{
+		Dictionary<string, string> res = new Dictionary<string, string>();
+		foreach(string k in items.Keys)
+		{
+			res.Add(k, (string)items[k]["path"]);
+		}
+		return res;
+	}
+
 	public string FromItemPathGiveID(string path)
 	{
 		// From the path of the item give the corresponding ID
-		var temp = items.ToDictionary(x => x.Value, x => x.Key);
+		var temp = itemsID.ToDictionary(x => x.Value, x => x.Key);
 		return temp[path];
 	}
 }
--- Scripts/Objects/Object.cs
+++ Scripts/Objects/Object.cs
@@ -4,7 +4,4 @@ using System;
 public abstract partial class Object : Node2D
 {
     public abstract string path { get; }
-    [Export] public bool stackable {get; protected set;}
-    [Export] public int stackSize {get; protected set;}
-    [Export] public Texture2D texture {get; protected set;}
 }
